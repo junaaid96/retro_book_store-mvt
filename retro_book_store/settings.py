724627c35bc6e74787598e8d5803c044b0f3f425
Md. Junaidul Islam <junaaid96@gmail.com>
2024-02-16 01:00:30 +0600

    Update settings.py

--- retro_book_store/settings.py
+++ retro_book_store/settings.py
@@ -91,12 +91,18 @@ WSGI_APPLICATION = 'retro_book_store.wsgi.application'
 
 
 # database
+#DATABASES = {
+#   'default': {
+#       'ENGINE': 'django.db.backends.sqlite3',
+#       'NAME': BASE_DIR / 'db.sqlite3',
+#   }
+#}
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.environ.get('sqlite:///db.sqlite3'))
 }
+
 
 # Email settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'

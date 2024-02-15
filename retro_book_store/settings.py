819727bb33a92e5d401644542d40017b142063bb
junaaid96 <info.junaidulislam@gmail.com>
2024-02-15 23:08:20 +0600

    settings updated

--- retro_book_store/settings.py
+++ retro_book_store/settings.py
@@ -31,7 +31,8 @@ SECRET_KEY = env('SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-CSRF_TRUSTED_ORIGINS = ['https://*.127.0.0.1']
+CSRF_TRUSTED_ORIGINS = [
+    'https://retro-book-store.onrender.com', 'https://*.127.0.0.1']
 
 LOGIN_URL = 'login'
 

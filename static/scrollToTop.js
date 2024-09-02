a2dd1e21f5ebc7dbe03789761a441b745534507f
junaaid96 <info.junaidulislam@gmail.com>
2024-09-03 01:20:19 +0600

    updates v2.3

---
+++ static/scrollToTop.js
@@ -0,0 +1,7 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//     document.querySelectorAll('form[action*="return_book"]').forEach(function(form) {
+//         form.addEventListener('submit', function(event) {
+//             window.scrollTo({ top: 0, behavior: 'smooth' });
+//         });
+//     });
+// });
